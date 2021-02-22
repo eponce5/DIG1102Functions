868d8cbbe34ad25169fbf835cc6aba27e9b440f3
eponce5 <eponce5@mail.valenciacollege.edu>
2021-02-21 23:43:42 -0500

    first commit

---
+++ functions.js
@@ -0,0 +1,40 @@
+
+
+function firstLetterName(name) {    
+    alert("The name "+name+ " starts with the letter "+name[0]);
+  }
+
+  firstLetterName("Ekaterina") 
+  
+  function name(parameter1, parameter2,) {
+
+  }
+
+{
+function divisibleByTwo(value) {
+  if (value %2 == 0)
+   return true;
+  else
+   return false;
+  }
+
+}
+function largestNum(arr) {
+  
+var arr = [1,3,5,6,23,97,67]
+var largest= arr[0];
+for (var i= 0; i <arr.length; i++) {
+    if (largest < arr [i]) {
+        largest = arr [i];
+    }
+}
+console.log(largestNum);
+
+
+//console.log (firstLetterName);
+
+
+//console.log (divisibleByTwo);
+
+
+}
